28d8a40fc94607a4fe345dd3225b57ab7432b2b4
Vanilson <vanilsonvl@gmail.com>
2015-05-27 09:24:10 -0300

    alteracao arquivos

--- Api_world/settings.py
+++ Api_world/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Api_world.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_world.sqlite3'),
     }
 }
 
